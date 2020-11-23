991d4d8a2ba93ac3fbcd04fe360a421612fecbf0
asim <asimarunava@gmail.com>
2020-11-23 19:06:39 +0530

    initial commit

---
+++ src/main/java/com/vortexmind/mymdb/MymdbAppApplication.java
@@ -0,0 +1,13 @@
+package com.vortexmind.mymdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymdbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymdbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vortexmind/mymdb/MymdbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexmind.mymdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymdbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
