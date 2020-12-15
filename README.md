86d382195642aa1ab043cbe2ace5ce9fe3c575de
asim <asimarunava@gmail.com>
2020-12-15 18:59:03 +0530

    Adding files for kafka integration.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mymdbk8s
 This Repo is to practise existing working springboot micro service to port to k8s.
+
+kubectl port-forward hello-podname 8080
--- src/main/java/com/vortexmind/mymdb/MymdbAppApplication.java
+++ src/main/java/com/vortexmind/mymdb/MymdbAppApplication.java
@@ -2,8 +2,10 @@ package com.vortexmind.mymdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.vortexmind")
 public class MymdbAppApplication {
 
 	public static void main(String[] args) {
